951b98f1d7d275b6c73b23ac4474dee0516c11e3
namtrung12345 <114908168+namtrung12345@users.noreply.github.com>
2023-08-30 15:18:07 +0700

    Add project files.

---
+++ Lion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asm2
+{
+    public class Lion : Animal
+    {
+        public Lion(double weight, string name) : base(weight, name)
+        {
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Lion:");
+            base.Show();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using asm2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Lion lion = new Lion(200, "Simba");
+        Tiger tiger = new Tiger(150, "Rajah");
+
+        Animal[] animals = { lion, tiger };
+
+        foreach (var animal in animals)
+        {
+            animal.Show();
+        }
+    }
+}
