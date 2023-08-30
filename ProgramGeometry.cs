0f432f4a608e35e7e30f7eb0b78c301375294792
namtrung12345 <114908168+namtrung12345@users.noreply.github.com>
2023-08-30 15:40:06 +0700

    Create ProgramGeometry.cs

---
+++ ProgramGeometry.cs
@@ -0,0 +1,16 @@
+using Geometry_Example;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cylinder cylinder = new Cylinder();
+
+        Console.WriteLine("ung dung dien tich va the tich cua xi lanh");
+        Console.WriteLine("ung dung dien tich va the tich cua xi lanh");
+
+        cylinder.Process();
+        cylinder.Result();
+    }
+}
